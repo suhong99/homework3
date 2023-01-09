f3288a8a79775dca85578e26389d430cec79e5c0
suhong99 <topic828@naver.com>
2023-01-10 03:19:34 +0900

    store족실수 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -13,7 +13,7 @@ import records from "../modules/recordsSlice";
  * 이것을 각각 모듈로 구현한 다음에 아래 코드로 2개의 모듈을 스토어에 연결해준 것 입니다.
  */
 const store = configureStore({
-  reducer: {},
+  // reducer: {},
   reducer: { records },
 });
 
